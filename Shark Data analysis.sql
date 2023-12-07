5bdb11292aaa64859a716b66bb2aa46f57daa083
AdeoyeTolu <Adeoyetoluwanimi18@gmail.com>
2023-12-07 14:00:34 +0000

    Create Shark Data analysis.sql

---
+++ Shark Data analysis.sql
@@ -0,0 +1,31 @@
+-- 1. What is the distribution of startups across different seasons of Shark Tank?
+
+-- 2. What industries were most commonly represented in the show?
+
+-- 3. How was the gender distribution among the entrepreneurs who pitched their startups?
+-- 4. Which city and state were most entrepreneurs from?
+
+-- 5. How many of the pitches actually got a deal with the sharks?
+-- 6. What was the total amount secured by startups in deals?
+-- 7. What was the total equity given up by startups in deals?
+
+
+--  8. How much did Barbara Corcoran typically invest in startups, and what equity did she receive?
+-- 9. How much did Mark Cuban typically invest in startups, and what equity did he receive?
+-- 10. How much did Lori Greiner typically invest in startups, and what equity did she receive?
+-- 11. How much did Robert Herjavec typically invest in startups, and what equity did he receive?
+-- 12. How much did Daymond John typically invest in startups, and what equity did he receive?
+-- 13. How much did Kevin O'Leary typically invest in startups, and what equity did he receive?
+
+-- 14. Which entrepreneur has secured the largest investment deal in "Shark Tank" history
+
+-- 15.  Which shark has invested the most money in businesses related to the food and beverage industry?
+
+-- 16. Are there any trends in the industries or product categories that tend to receive higher valuation offers from the sharks?
+
+-- 17. Did startups with a larger number of entrepreneurs tend to secure more favorable deals?
+
+
+-- 18. Which industry or product category receives the highest average deal valuation from the sharks?
+-- 19. How do the total investments made by male and female entrepreneurs compare?
+-- 20. Are there any correlations between the amount of equity offered and the probability of securing a deal?
